06b7f93053a214283defa90806f2fd5be68e4e50
zksystem <zksystem@yahoo.com>
2023-09-29 15:18:56 +0300

    yandex analytics

--- Tracker/Tracker/Statistics/Analytics.swift
+++ Tracker/Tracker/Statistics/Analytics.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import YandexMobileMetrica
+
+struct Analytics {
+    static func setup() {
+        guard let config = YMMYandexMetricaConfiguration(apiKey: "api key") else {
+            return
+        }
+        YMMYandexMetrica.activate(with: config)
+    }
+    
+    func report(event: String, params: [AnyHashable: Any]) {
+        YMMYandexMetrica.reportEvent(event, parameters: params) {
+            error in print("Report event error: %@", error.localizedDescription)
+        }
+    }
+    
+}
